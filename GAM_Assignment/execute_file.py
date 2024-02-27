1fa264826b40b17987fb3de6ea4d36d2c16da1ae
pravinkumardm <dmpravinkumar@gmail.com>
2024-02-27 23:30:37 +0530

    Added Word Document

--- GAM_Assignment/execute_file.py
+++ GAM_Assignment/execute_file.py
@@ -13,19 +13,20 @@ def iterate_ERgraphs(n, p, iterations):
         model_lc.append(len(lcc))
         # print(f'np value = {n*p} | Largest Connected Component in the graph : {len(lcc)} | Number of Triangles in the graph: {ergraph.get_triangles()} ')
     if n*p < 1:
-        print(f'\tTrue size of LCC (Average): {sum(model_lc) / len(model_lc)} : From Formula(O(log(n))): {math.log(n)} Truesize > O(log(n)) : {sum(model_lc) / len(model_lc) > math.log(n)}')
+        print(f'\tTrue size of LCC (Mean for {iterations} iterations): {sum(model_lc) / len(model_lc)} : From Formula(O(log(n))): {math.log(n)} Truesize > O(log(n)) : {sum(model_lc) / len(model_lc) > math.log(n)}')
     elif n*p == 1:
-        print(f'\tTrue size of LCC (Average): {sum(model_lc) / len(model_lc)} : From Formula(n^(2/3)): {n**(2/3)} Truesize == n^(2/3) : {sum(model_lc) / len(model_lc) == int(n**(2/3))}')
+        print(f'\tTrue size of LCC (Mean for {iterations} iterations): {sum(model_lc) / len(model_lc)} : From Formula(n^(2/3)): {n**(2/3)} Truesize == n^(2/3) : {sum(model_lc) / len(model_lc) == int(n**(2/3))}')
     else:
-        print(f'\tTrue size of LCC (Average): {sum(model_lc) / len(model_lc)} : From Formula(n): {n} Truesize == n : {sum(model_lc) / len(model_lc) == int(n)}')
-    print(f'\tMean for 100 iterations: {sum(model_lc) / len(model_lc)}')
-    print(f'===================================================')
+        print(f'\tTrue size of LCC (Mean for {iterations} iterations): {sum(model_lc) / len(model_lc)} : From Formula(n): {n} Truesize == n : {sum(model_lc) / len(model_lc) == int(n)}')
+    # print(f'\tMean for 100 iterations: {sum(model_lc) / len(model_lc)}')
 
 
 iterate_ERgraphs(100, 0.04, 100)
 iterate_ERgraphs(100, 0.5, 100)
 
 
+
+print(f'===================================================')
 adjmat = np.array([
         [0,1,1,1,0,0,0,0,0,0],
         [0,0,1,0,0,0,0,0,1,0],
